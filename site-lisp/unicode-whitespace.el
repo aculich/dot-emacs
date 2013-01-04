c546acced6086686b1addb466b7315fca0589b86
Aaron Culich <aculich@eecs.berkeley.edu>
2013-01-04 02:53:01 -0800

    Fixed called-interactively-p due to subrp Emacs 24.3 incompatibility

--- site-lisp/unicode-whitespace.el
+++ site-lisp/unicode-whitespace.el
@@ -583,7 +583,7 @@ See `unicode-whitespace-toggle-echo' to activate."
 
 Optional KIND is as documented at `called-interactively-p'
 in GNU Emacs 24.1 or higher."
-  (if (eq 0 (cdr (subr-arity (symbol-function 'called-interactively-p))))
+  (if (string< emacs-version "24.1")
       '(called-interactively-p)
     `(called-interactively-p ,kind)))
 
